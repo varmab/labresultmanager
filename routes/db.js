fafa3ebc0501b51491fd66784c57a9e6c1cd2a0b
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2019-10-19 12:35:21 +0530

    Implemented insert query for Transactrion

--- routes/db.js
+++ routes/db.js
@@ -1,1 +1,93 @@
-exports.dbQueries = data => {}
+var sql = require("mssql")
+let PromisePool = require("es6-promise-pool")
+require("dotenv").config()
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  options: {
+    encrypt: true,
+    database: process.env.DB_DATABASE
+  }
+}
+console.log("db config", config)
+
+exports.dbQueries = obj => {
+  let { orc, msh, pid, obx } = obj
+  // xrxQuestResultTransaction fields
+  const { transaction_id, vendor_accession_no } = orc
+  const { message_control_id, lab_result_send_datetime } = msh
+  const {
+    vendor_order_referenceno,
+    patid,
+    vendor_onfile_pat_lastname,
+    vendor_onfile_pat_firstname,
+    vendor_onfile_pat_dob,
+    vendor_onfile_pat_sex,
+    vendor_onfile_pat_ssn
+  } = pid
+  const RetrievedDateTime = new Date()
+  const ReviewedDateTime = new Date()
+  // var conn = `Server=${config.server},1433;Database=${config.options.database};User Id=${config.user};Password=${config.password};Encrypt=true`
+  var connection = new sql.ConnectionPool(config)
+  connection.close()
+  connection
+    .connect()
+    .then(() => {
+      console.log("connection establisheddd")
+      var request = new sql.Request(connection)
+      request
+        .query(
+          "insert into xrxQuestResultTransaction (TransactionId, VendorAccessionNo, MessageControlId, LabResultSendDateTime, VendorOrderReferenceNo, PatId, VendorOnFilePatLastName, VendorOnFilePatFirstName, VendorOnFilePatDOB, VendorOnFilePatSex, VendorOnFilePatSSN) Values(" +
+            transaction_id +
+            "," +
+            vendor_accession_no +
+            "," +
+            message_control_id +
+            "," +
+            lab_result_send_datetime +
+            "," +
+            vendor_order_referenceno +
+            "," +
+            patid +
+            "," +
+            vendor_onfile_pat_lastname +
+            "," +
+            vendor_onfile_pat_firstname +
+            "," +
+            vendor_onfile_pat_dob +
+            "," +
+            vendor_onfile_pat_sex +
+            "," +
+            vendor_onfile_pat_ssn +
+            ")"
+        )
+        .then(recordSet => {
+          console.log("recordSet", recordSet)
+          dbConn.close()
+        })
+        .catch(err => {
+          console.log(err)
+          dbConn.close()
+        })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  // xrxQuestResultObservationResult fields
+  obx.forEach(result => {
+    const {
+      labresult_valuetype,
+      labresult_analyte_number,
+      labresult_analyte_name,
+      labresult_measure_units,
+      labresult_normal_range,
+      labresult_normalcy_status,
+      labresult_status,
+      labresult_datetime,
+      labresult_fillerId
+    } = result
+  })
+}
--- routes/hl7.js
+++ routes/hl7.js
@@ -3,6 +3,7 @@ const Reader = require("hl7js").Reader
 const reader = new Reader("BASIC")
 const parser = require("@rimiti/hl7-object-parser")
 const s12Mapping = require("./s12.json")
+const queries = require("./db")
 
 exports.parseHl7Files = () => {
   const filesFolder = __basedir + "/files/"
@@ -12,9 +13,12 @@ exports.parseHl7Files = () => {
       fs.readFile(hl7_file_path, function(err, buffer) {
         reader.read(buffer.toString(), function(err, hl7Data) {
           if (err) {
-            console.log("err@@", err)
+            logger.log({
+              level: "error",
+              message: err
+            })
           } else {
-            fetchValues(hl7Data.segments[0].rawData)
+            mapValues(hl7Data.segments[0].rawData)
           }
         })
       })
@@ -22,33 +26,8 @@ exports.parseHl7Files = () => {
   })
 }
 
-function fetchValues(rawMessage) {
+function mapValues(rawMessage) {
   const obj = parser.decode(rawMessage, s12Mapping)
-  // xrxQuestResultTransaction fields
-  const TransactionId = obj.orc.transaction_id
-  const VendorAccessionNo = obj.orc.vendor_accession_no
-  const MessageControlId = obj.msh.message_control_id
-  const LabResultSendDateTime = obj.msh.lab_result_send_datetime
-  const VendorOrderReferenceNo = obj.pid.vendor_order_referenceno
-  const PatId = obj.pid.patid
-  const VendorOnFilePatLastName = obj.pid.vendor_onfile_pat_lastname
-  const VendorOnFilePatFirstName = obj.pid.vendor_onfile_pat_firstname
-  const VendorOnFilePatDOB = obj.pid.vendor_onfile_pat_dob
-  const VendorOnFilePatSex = obj.pid.vendor_onfile_pat_sex
-  const VendorOnFilePatSSN = obj.pid.vendor_onfile_pat_ssn
-  const RetrievedDateTime = new Date()
-  const ReviewedDateTime = new Date()
-
-  // xrxQuestResultObservationResult fields
-  obj.obx.forEach(result => {
-    const LabResultValueType = result.labresult_valuetype
-    const LabResultAnalyteNumber = result.labresult_analyte_number
-    const LabResultAnalyteName = result.labresult_analyte_name
-    const LabResultMeasureUnits = result.labresult_measure_units
-    const LabResultNormalRange = result.labresult_normal_range
-    const LabResultNormalcyStatus = result.labresult_normalcy_status
-    const LabResultStatus = result.labresult_status
-    const LabResultDateTime = result.labresult_datetime
-    const LabResultFillerId = result.labresult_fillerId
-  })
+  // Passing mapped values to queries
+  queries.dbQueries(obj)
 }
