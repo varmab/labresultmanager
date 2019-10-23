7d1023ca16fea35038cac11d3cb0af5082e68560
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2019-10-23 23:44:14 +0530

    minor changes

--- routes/sftp.js
+++ routes/sftp.js
@@ -4,57 +4,68 @@ let sftp = new Client()
 require("dotenv").config()
 
 exports.downloadHl7Files = () => {
-  return new Promise((resolve,reject)=>{
-    logger.log({ level:"info",message:"connecting to sftp"})
+  return new Promise((resolve, reject) => {
+    logger.log({ level: "info", message: "connecting to sftp" })
     sftp
-    .connect({
-      host: process.env.AEGIS_HOST,
-      port: process.env.AEGIS_PORT,
-      username: process.env.AEGIS_USERNAME,
-      password: process.env.AEGIS_PASSWORD
-    })
-    .then(() => {
-      logger.log({ level:"info",message:"sftp connection established"})
-      return sftp.list(process.env.AEGIS_REMOTE_PATH)
-    })
-    .then(files => {
-      var downloads = []
-      var localFiles=[]
-      files.forEach(file => {
-        const remoteFilename = process.env.AEGIS_REMOTE_PATH + file.name
-        const localFilename = __basedir + "/files/" + file.name
-        localFiles.push(localFilename);
-        downloads.push(sftp.get(remoteFilename, localFilename))
+      .connect({
+        host: process.env.AEGIS_HOST,
+        port: process.env.AEGIS_PORT,
+        username: process.env.AEGIS_USERNAME,
+        password: process.env.AEGIS_PASSWORD
       })
-      Promise.all(downloads)
-        .then(() => {
-            var filesToDelete=[];
-            files.forEach(file => {
-              const remoteFilename = process.env.AEGIS_REMOTE_PATH + file.name
-              filesToDelete.push(sftp.delete(remoteFilename))
-            })
+      .then(() => {
+        logger.log({ level: "info", message: "sftp connection established" })
+        return sftp.list(process.env.AEGIS_REMOTE_PATH)
+      })
+      .then(files => {
+        if (files.length > 0) {
+          var downloads = []
+          var localFiles = []
+          files.forEach(file => {
+            const remoteFilename = process.env.AEGIS_REMOTE_PATH + file.name
+            const localFilename = __basedir + "/files/" + file.name
+            localFiles.push(localFilename)
+            downloads.push(sftp.get(remoteFilename, localFilename))
+          })
+          Promise.all(downloads)
+            .then(() => {
+              var filesToDelete = []
+              files.forEach(file => {
+                const remoteFilename = process.env.AEGIS_REMOTE_PATH + file.name
+                filesToDelete.push(sftp.delete(remoteFilename))
+              })
 
-            Promise.all(filesToDelete)
-            .then(()=>{
-              sftp.end();
-              logger.log({ level:"info",message:"completed downloading files"})
-              resolve(localFiles)
+              Promise.all(filesToDelete).then(() => {
+                sftp.end()
+                logger.log({
+                  level: "info",
+                  message: "completed downloading files"
+                })
+                resolve(localFiles)
+              })
             })
-        })
-        .catch((err) => {
-          sftp.end()
-          let errorMessage="Failed to finish downloading:" +  JSON.stringify(err);
-          logger.log({ level:"error",message:errorMessage})
-          let error=new Error(errorMessage)
-          reject(error)
-        })
-    })
-    .catch(err => {
-      let errorMessage="Failed to download files:" +  JSON.stringify(err);
-      logger.log({ level:"error",message:errorMessage})
-      let error=new Error(errorMessage)
-      reject(error)
-    })
+            .catch(err => {
+              sftp.end()
+              let errorMessage =
+                "Failed to finish downloading:" + JSON.stringify(err)
+              logger.log({ level: "error", message: errorMessage })
+              let error = new Error(errorMessage)
+              reject(error)
+            })
+        } else {
+          logger.log({
+            level: "info",
+            message: "There are no files available on server"
+          })
+          resolve
+        }
+      })
+      .catch(err => {
+        let errorMessage = "Failed to download files:" + JSON.stringify(err)
+        logger.log({ level: "error", message: errorMessage })
+        let error = new Error(errorMessage)
+        reject(error)
+      })
   })
 }
 
--- service.js
+++ service.js
@@ -1,21 +1,19 @@
 const cron = require("node-cron")
 let logger = require("./routes/logger")
 let sftp = require("./routes/sftp")
-let hl7=require('./routes/hl7')
-let db=require('./routes/db')
+let hl7 = require("./routes/hl7")
+let db = require("./routes/db")
 
 global.__basedir = __dirname
 
 class LabResultManager {
   static run() {
     //Download Files to local folder
-    sftp.downloadHl7Files()
-    .then((localFiles)=>{
-      localFiles.map((file)=>{
-        hl7.parseHl7File(file)
-        .then((hl7Obj)=>{
-          db.saveToDB(hl7Obj).then((value)=>{
-            logger.log({level:"info",message:value})
+    sftp.downloadHl7Files().then(localFiles => {
+      localFiles.map(file => {
+        hl7.parseHl7File(file).then(hl7Obj => {
+          db.saveToDB(hl7Obj).then(value => {
+            logger.log({ level: "info", message: value })
           })
         })
       })
